fd6cd8fbce58906b94d8f66dc510632a12307ad3
Satadru Roy <satadru.roy98@gmail.com>
2023-08-27 22:18:59 +0530

    password checker to understand loops better

---
+++ password_checker.py
@@ -0,0 +1,8 @@
+password = input("Enter your password: ")
+
+while password != "opensesame":
+    print("Wrong password, try again!")
+    password = input("Enter your password: ")
+
+print("Welcome to secret town!")
+
